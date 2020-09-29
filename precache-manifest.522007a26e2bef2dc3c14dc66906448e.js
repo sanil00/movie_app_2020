d3eb765bc09fddd7c247d0ef09ebcdecb912eb0d
unknown <reyel3@cheongshim.com>
2020-09-29 10:24:59 +0900

    Updates

---
+++ precache-manifest.522007a26e2bef2dc3c14dc66906448e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97bd8fa1d3e96a5606d7a59cf406735c",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "f7fc60f7e4b03119a08d",
+    "url": "/movie_app_2020/static/css/main.647acd5b.chunk.css"
+  },
+  {
+    "revision": "dcbc1ad3746847d335f9",
+    "url": "/movie_app_2020/static/js/2.968ebb96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.968ebb96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7fc60f7e4b03119a08d",
+    "url": "/movie_app_2020/static/js/main.9ab3cd96.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
